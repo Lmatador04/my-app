ab0d99302ad9c3d8739240affb1acef117277592
wilfried fonkeng <afk1995wilfried@gmail.com>
2020-11-11 11:53:04 +0100

    react

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Container, Navbar,Nav} from 'react-bootstrap'
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="ml-auto">
+      <Nav.Link href="/cart"><i className= "fas fa-shopping-cart"></i>   Cart</Nav.Link>
+      <Nav.Link href="/login"> <i className= "fas fa-user"></i>  Sign In</Nav.Link>
+      
+    </Nav>
+    </Navbar.Collapse>
+    </Container>
+</Navbar>
+                
+  
+        </header>
+    )
+}
+
+export default Header
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Row, Col} from 'react-bootstrap'
+import Products from '../products'
+import Product from '../components/Product'
+
+const HomeScreen = () => {
+    return (
+        <div>
+           <h1> Latest Products</h1> 
+           <Row>
+               {Products.map(
+                   (product) =>
+                   
+                   <Col sm={12} md={6} lg={4} xl={3} key = {product._id}>
+                       <Product   product = {product} />
+                   </Col>
+                  
+               )}
+
+           </Row>
+        </div>
+    )
+}
+
+export default HomeScreen
