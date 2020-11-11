bab033531b6dceb5be74a434930da4656a9691f2
wilfried fonkeng <afk1995wilfried@gmail.com>
2020-11-11 12:32:39 +0100

    react

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import {Container} from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+
 function App() {
   return (
-    <Router>
+    <>
     <Header />
     <main className= "py-3" >
     <Container>
-    <Route path='/' component= {HommeScreen} exact />
+    <HomeScreen />
     </Container>
     
     </main>
     <Footer />
-    </Router>
+    </>
   );
 }
 
